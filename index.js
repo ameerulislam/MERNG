0ec2156365c4b4584692793c7336e10c621987a3
Ameer Ul Islam <ameerulislam@gmail.com>
2022-02-20 20:29:06 +0600

    mongodb Connected

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const {ApolloServer} = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+const {MONGODB} = require('./config')
 
-
+// Defined gql to acomodate Query
 const typeDefs = gql`
     type Query{
         sayHi: String!
     }
 `
-
+// Defined resolver of the query that basically says what a query needs to do.
 const resolvers = {
     Query: {
         sayHi: () => 'Hello Ameer'
     }
+    
 }
-
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
 
-server.listen({port: 5000}).then(res => {
-    console.log(`server running at ${res.url}`);
-});
+mongoose.connect(MONGODB, {useNewUrlParser: true})
+    .then(()=>{
+        console.log('MongoDB Connected');
+        return server.listen({port: 5000});
+    }).then(res => {
+        console.log(`server running at ${res.url}`);
+    });
+
+
+
