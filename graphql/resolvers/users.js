54c54bdd5b08261aa2d33b62d186789689447141
Ameer Ul Islam <ameerulislam@gmail.com>
2022-02-22 21:08:27 +0600

    username duplication check added

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,15 +1,24 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const {UserInputError} = require('apollo-server');
 
 const {SECRET_KEY} = require('../../config')
 const User = require('../../models/User');
 
 module.exports = {
     Mutation: {
-        async register(_, {registerInput: {username, email, password, confirmPassword}}, contex, info){
+        async register(_, {registerInput: {username, email, password, confirmPassword}}){
             // TODO: Validate User Data
             // TODO: Make Sure email doesn't already exists
-            // TODO: Hash password and create authentication token
+            const user = await User.findOne({ username });
+            if(user){
+                throw new UserInputError('Username is Taken', {
+                    errors: {
+                        username: 'This username is taken'
+                    }
+                });
+            }
+            // DONE: Hash password and create authentication token
             password = await bcrypt.hash(password, 12);
             const newUser = new User({
                 email,
